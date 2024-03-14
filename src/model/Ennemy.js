6a6be791f1c05e75cd7710eb90093ba1c64d62c9
florian delbe <florian.delbe.etu@univ-lille.fr>
2024-03-14 15:58:28 +0100

    test au vert

---
+++ src/model/Ennemy.js
@@ -0,0 +1,13 @@
+export default class{
+    ennemy_name;
+    speed;
+    pattern;
+
+    constructor(name,speed,pattern){
+        this.ennemy_name=name;
+        this.speed=speed;
+        this.pattern=pattern;
+    }
+
+
+}
