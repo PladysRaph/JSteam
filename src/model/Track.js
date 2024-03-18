0bd0bcc9cafa91a039ab5ec715aa1c2f23dbe593
Raphaël Pladys <raphael.pladys.etu@univ-lille.fr>
2024-03-18 13:02:14 +0100

    implement Track.js

---
+++ src/model/Track.js
@@ -0,0 +1,9 @@
+export default class Track {
+    constructor(x, y, time) {
+        // Vecteur de déplacement
+        this.x = x;
+        this.y = y;
+        // Temps durant lequel effectuer le déplacement
+        this.time = time;
+    }
+}
