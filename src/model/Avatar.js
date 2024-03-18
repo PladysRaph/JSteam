25286363271e69b4de114b7d51b520e723bde321
Raphaël Pladys <raphael.pladys.etu@univ-lille.fr>
2024-03-18 13:34:42 +0100

    Test for Entity

--- src/model/Avatar.js
+++ src/model/Avatar.js
@@ -4,8 +4,6 @@ export default class Avatar {
     static renderImage(url) {
         let image = new Image();
         image.src = url;
-        image.width = 100;
-        image.height = 100;
         return image;
     }
 }
