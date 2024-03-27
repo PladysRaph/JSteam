30b20e1a816b3bf33faae137ed16e442cfba4d9b
Raphaël Pladys <raphael.pladys.etu@univ-lille.fr>
2024-03-27 09:56:10 +0100

    Implements Bullet.js

--- src/test/Bullet.test.js
+++ src/test/Bullet.test.js
@@ -1,16 +1,16 @@
 import assert from 'node:assert/strict';
 import {describe, it } from 'node:test';
 
-import Ennemy from '../model/Enemy.js';
+import Bullet from '../model/Bullet.js';
 import Avatar from '../model/Avatar.js'
 import Track from '../model/Track.js'
 
 describe('Create Bullet',() => {
-        it('shoud create new bullet', () =>{
+        it('should create new bullet', () =>{
             const bullet = new Bullet(
                 'Bullet 1', 0, 0, 10,
                 new Avatar('public/assets/img/red-pearl-bullet.png', 32, 32),
-                null);
+                null, 1, 500);
 
             // Vérifie que bulllet est bien créé
             assert.notEqual(bullet, undefined);
@@ -23,19 +23,16 @@ describe('Create Bullet',() => {
             assert.equal(bullet.avatar.height, 32);
             assert.equal(bullet.x, 0);
             assert.equal(bullet.y, 0);
-            assert.equal(bullet.pattern[0].x, -1);
-            assert.equal(bullet.pattern[0].y, 0);
-            assert.equal(bullet.pattern[0].time, 1);
         }
     )}
 )
 
 describe('Displace Bullet',() => {
-    it('shoud displace a bullet', () =>{
-        const bullet = new Ennemy(
+    it('should displace a bullet', () =>{
+        const bullet = new Bullet(
             'Bullet 1', 200, 0, 1,
             new Avatar('public/assets/img/red-pearl-bullet.png', 32, 32),
-            null);
+            null, 1, 50);
 
         // Vérifie la position de base du bullet
         assert.equal(bullet.x, 200);
@@ -43,8 +40,10 @@ describe('Displace Bullet',() => {
 
         // Déplace le bullet comme s'il avait bougé pendant 100 frames puis vérifie que le déplacement a bien été effectué
         bullet.skipTime(100);
-        assert.equal(bullet.x, 100);
-        assert.equal(bullet.y, 0);
+        assert.equal(bullet.arrX[0], 100);
+        assert.equal(bullet.arrY[0], 0);
+        assert.equal(bullet.arrX[1], 150);
+        assert.equal(bullet.arrY[1], 0);
     }
 )}
 )
