810177100c4f9cb6dc203fe3ea32a4b580bbad6a
Raphaël Pladys <raphael.pladys.etu@univ-lille.fr>
2024-03-29 14:03:12 +0100

    doc for bullet and enemy

--- src/model/Player.js
+++ src/model/Player.js
@@ -5,10 +5,12 @@ import Entity from './Entity.js'
 export default class Player extends Entity {
     constructor(name, avatar, bullet = null) {
         super(name, 0, 0, 2, avatar);
+        // PVs du joueur
         this.hp = 50;
         // Facteurs de vitesse
         this.xFactor = 0;
         this.yFactor = 0;
+        // Balle qu'utilise le joueur
         if (bullet == null) 
             this.bullet = new Bullet('Red pearl bullet', this.x, this.y, 10,
                 new Avatar('public/assets/img/red-pearl-bullet.png', 16, 16),
