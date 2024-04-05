f8626272954e6712d9cd03ab5dcd6f0a1fcd4693
florian delbe <florian.delbe.etu@univ-lille.fr>
2024-04-05 16:28:57 +0200

    test Player et fix test bullet +ajoute test de delete

--- src/test/Bullet.test.js
+++ src/test/Bullet.test.js
@@ -22,6 +22,10 @@ describe('Create Bullet',() => {
             assert.equal(bullet.avatar.height, 32);
             assert.equal(bullet.x, 0);
             assert.equal(bullet.y, 0);
+
+            // Vérifie que les arrays ont été initialisé et sont vide
+            assert.equal(bullet.arrX.length,0); 
+            assert.equal(bullet.arrY.length,0);
         }
     )}
 )
@@ -30,19 +34,56 @@ describe('Displace Bullet',() => {
     it('should displace a bullet', () =>{
         const bullet = new Bullet(
             'Bullet 1', 200, 0, 1,
-            new Avatar('public/assets/img/red-pearl-bullet.png', 32, 32), 1, 50);
+            new Avatar('public/assets/img/red-pearl-bullet.png', 32, 32), 1, 10,50);
 
         // Vérifie la position de base du bullet
         assert.equal(bullet.x, 200);
         assert.equal(bullet.y, 0);
 
+        // Vérifie les autre variable du bullet
+
+        assert.equal(bullet.damage,10);
+        assert.equal(bullet.cooldown,50);
+
         // Déplace le bullet comme s'il avait bougé pendant 100 frames puis vérifie que le déplacement a bien été effectué
-        bullet.skipTime(100);
+        bullet.skipFrame(100);
+        assert.equal(bullet.arrX.length,2);
+        assert.equal(bullet.arrY.length,2);
         assert.equal(bullet.arrX[0], 100);
         assert.equal(bullet.arrY[0], 0);
         assert.equal(bullet.arrX[1], 150);
         assert.equal(bullet.arrY[1], 0);
+
     }
 )}
+)
+
+describe('Delete Bullet',() => {
+    it('should delete a bullet form array',() => {
+        const bulletTest = new Bullet(
+            'Bullet 1', 200, 0, 1,
+            new Avatar('public/assets/img/red-pearl-bullet.png', 32, 32), 1, 10,50);
+        
+        // Vérifie que bullet est bien créé
+        assert.notEqual(bulletTest, undefined);
+        assert.notEqual(bulletTest, null);
+
+        
+        //passer le temps pour ajouter des bullets
+        bulletTest.skipFrame(100);
+        assert.equal(bulletTest.arrX.length,2);
+        assert.equal(bulletTest.arrY.length,2);
+
+        // Tue la bullet 
+        bulletTest.delete(0);
+        
+        //Vérifie que la bullet a été détruite et que la bullet à l'index 0 a bien été remplacé par celle a l'index 1
+        assert.equal(bulletTest.arrX.length,1);
+        assert.equal(bulletTest.arrY.length,1);
+        assert.equal(bulletTest.arrX[0], 150);
+        assert.equal(bulletTest.arrY[0], 0);
+    } )
+
+}
 )
 
--- src/test/Player.test.js
+++ src/test/Player.test.js
@@ -1,8 +1,32 @@
-// import assert from 'node:assert/strict';
-// import { describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+import Avatar from '../model/Avatar.js';
+import { describe, it } from 'node:test';
+import Player from '../model/Player.js';
 
-// describe('...',() => {
-//     it('...', () => {
-//         assert.strictEqual(expected, value);
-//     });
-// });
+describe('Create Player', () => {
+    it('should create a new player', () => {
+        const playerTest = new Player('test',new Avatar('public/assets/img/balloon-ship.png', 100, 100));
+        
+        //vérifie que l'ennemi a bien été créer
+        assert.notEqual(playerTest,undefined);
+        assert.notEqual(playerTest,null);
+
+        //vérifie que les variable on bien été initialisé avec les valeurs passés dans le contructeur parents
+        assert.equal(playerTest.name, 'test');
+        assert.equal(playerTest.avatar.url, 'public/assets/img/balloon-ship.png');
+        assert.equal(playerTest.avatar.width, 100);
+        assert.equal(playerTest.avatar.height, 100);
+        assert.equal(playerTest.x,0);
+        assert.equal(playerTest.y,0);
+
+        //vérifie les variable propre a Player sauf les dégats car testé dans Bullet.test.js
+        assert.equal(playerTest.hp,50);
+        assert.equal(playerTest.xFactor,0);
+        assert.equal(playerTest.yFactor,0);
+        assert.equal(playerTest.duration,0);
+        assert.equal(playerTest.kills,0);
+        assert.equal(playerTest.score,0);
+        assert.equal(playerTest.isShooting,false);
+
+    })
+})
